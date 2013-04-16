97664e7cbbb5af3eefe27784095efb3fe082b036
Victor Ingvarsson <1337hackern@hotmail.se>
2013-04-16 12:55:08 +0200

    kommentar

--- Forum Rollspel/visatabell.php
+++ Forum Rollspel/visatabell.php
@@ -1,8 +1,11 @@
-﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" 
+﻿<?php
+include 'connect.php';
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" 
 		"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="sv" lang="sv">    
     <head>
-        <meta http-equiv="Content-Type" content="text/html charset=utf-8" />
+        <meta http-equiv="Content-Type" content="text/html charset=latin-1" />
         <link rel="stylesheet" title="magnum" type="text/css" href="./CSS/magnum.css" />
 		<link rel="alternate stylesheet" title="none" type="text/css" href="../CSS/empty.css.css" />	  
         <title>Forum Rollspel</title>	
@@ -29,13 +32,13 @@
 				<h1 class="dokument-item-header">Inlägg och dylikt</h1>
 				<p class="info">Här kommer man att se alla inlägg och dylikt.<br> Site under construction.</p>
 				<?php
-         $query = "SELECT id, thread, sender FROM meddelande";
+         $query = "SELECT id, thread, sender, subject, msg FROM meddelande";
          //echo '<em> ' . $query . ' </em>';
          $result = mysql_query($query);
          if ($result === false) {
 	         echo '<strong> Error when you asked a question to your databas. ' . mysql_errno . ' : <br />' . mysql_error . '</strong>';
         }
-
+		
          $num=mysql_numrows($result);
          if($num==0) {
              echo '<strong>Your question is empty</strong>';
@@ -44,7 +47,7 @@
              echo "<ul>";
              for ($i=0;$i<$num;$i++) {
                  $temp = mysql_fetch_array($result);
-	             echo "<li>" . $temp["namn"] . "</li>";
+	             echo "<li>" . $temp["subject"] . "  :  ". $temp["msg"] . "</li>";
              }
              echo "</ul>";
         }
