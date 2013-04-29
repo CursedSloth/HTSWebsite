70e756b059542369797c8c7c4fd19ca01c709f9d
Victor Ingvarsson <1337hackern@hotmail.se>
2013-04-29 11:11:00 +0200

    kommentar

---
+++ Forum_Rollspel/insertMeddelande.php
@@ -0,0 +1,65 @@
+﻿<?php
+include '../Forum_Rollspel/connect.php';
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" 
+		"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="sv" lang="sv">    
+    <head>
+        <meta http-equiv="Content-Type" content="text/html charset=latin-1" />
+        <link rel="stylesheet" title="magnum" type="text/css" href="./CSS/magnum.css" />
+		<link rel="alternate stylesheet" title="none" type="text/css" href="../CSS/empty.css.css" />	  
+        <title>Forum Rollspel</title>	
+    </head>
+    <body>
+        <div id="content">
+            <div id="top">
+               <div id="banner">Rollspel, På forum</div>
+            </div>
+            <div id="left">
+					<div class="meny">
+						<ul>
+						<li><a href="Login.php">Logga in</a></li>
+						<li><a href="sida2.php">Meny 2</a></li>
+						<li><a href="sida3.php">Meny 3</a></li>
+						<li><a href="sida4.php">Meny 4</a></li>
+						<li><a href="sida5.php">Meny 5</a></li>
+						</ul>
+					</div>
+            </div>
+            <div id="center-right">
+				Forum Rollspel
+                <div id="info">
+				<h1 class="dokument-item-header">Inlägg och dylikt</h1>
+				<p class="info">Här kommer man att se alla inlägg och dylikt.<br> Site under construction.</p>
+<?php
+         $query = "SELECT thread, sender, subject, msg FROM meddelande";
+         //echo '<em> ' . $query . ' </em>';
+         $result = mysql_query($query);
+         if ($result === false) {
+	         echo '<strong> Error when you asked a question to your databas. ' . mysql_errno . ' : <br />' . mysql_error . '</strong>';
+        }
+		
+         $num=mysql_numrows($result);
+         if($num==0) {
+             echo '<strong>Your question is empty</strong>';
+         }
+         else {
+             echo "<ul>";
+             for ($i=0;$i<$num;$i++) {
+                 $temp = mysql_fetch_array($result);
+	             echo "<li>" . $temp["subject"] . "  :  ". $temp["msg"] . "</li>";
+             }
+             echo "</ul>";
+        }
+?>
+</div>
+</div>
+
+
+			<div id="footer">
+				<p> &copy; 2013 Victor Ingvarsson.# 
+				</p>
+			</div>
+        </div>
+    </body>
+</html>
--- Snippet/login.php
+++ Snippet/login.php
@@ -1,16 +1,19 @@
 ﻿<!-- Kom ihåg att logga in till databasen först! -->
 <?php
-         $query = "SELECT name FROM user WHERE name='$_POST[myUser]' AND password='$_POST[myPassword]'";
-         //echo '<em> ' . $query . ' </em>';
+include '../Forum_Rollspel/connect.php';
+?>
+<?php
+         $query = "SELECT namn FROM sender WHERE namn='$_POST[myUser]' AND password='$_POST[myPassword]'";
+         echo '<em> ' . $query . ' </em>';
          $result = mysql_query($query);
 		 session_start();
 		 session_unset();
          if (mysql_numrows($result) == 1) {
 		      $_SESSION['session_user']=$_POST[myUser];
-			  header('Location: index.php');
+			  // header('Location: index.php');
 		 }
 		 else {
-			  header('Location: login.html');
+			 // header('Location: login.html'); 
 		 }
 		
 ?>
